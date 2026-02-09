385cd98fd25ea4e716fcdad2d3ed18f4e99ef7d7
mosewear <info@mosewear.nl>
2026-02-09 20:33:15 +0100

    Fix: Trustpilot script exact zoals Trustpilot vereist - direct inline script tag in head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -173,15 +173,13 @@ export default async function RootLayout({
           />
         </noscript>
         {/* Trustpilot JavaScript Integration - Required for domain verification */}
-        <Script
-          id="trustpilot-integration"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
-                a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
-                f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
-                tp('register', 'AAbEsaY7hRnD5xEZ');
+(function(w,d,s,r,n){w.TrustpilotObject=n;w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)};
+    a=d.createElement(s);a.async=1;a.src=r;a.type='text/java'+s;f=d.getElementsByTagName(s)[0];
+    f.parentNode.insertBefore(a,f)})(window,document,'script', 'https://invitejs.trustpilot.com/tp.min.js', 'tp');
+    tp('register', 'AAbEsaY7hRnD5xEZ');
             `,
           }}
         />
